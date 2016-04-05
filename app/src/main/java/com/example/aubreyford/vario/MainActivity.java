318bbafda68889ce559607e72cd23b8232d39e3d
Aubrey Ford <musicaubrey@gmail.com>
2016-04-04 22:15:10 -0600

    initial commit

---
+++ app/src/main/java/com/example/aubreyford/vario/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.aubreyford.vario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Random;
+import io.sule.gaugelibrary.GaugeView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GaugeView mGaugeView;
+    private final Random RAND = new Random();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGaugeView = (GaugeView) findViewById(R.id.gauge_view);
+        mGaugeView.setTargetValue(50);
+    }
+
+
+}
